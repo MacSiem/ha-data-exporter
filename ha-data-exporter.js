c98870cf26bb03f6f599048313404faa04432f9c
MacSiem <maciek.sieminski@gmail.com>
2026-03-18 10:34:37 +0100

    Fix flickering: increase throttle to 10s + add state hash check

--- ha-data-exporter.js
+++ ha-data-exporter.js
@@ -38,7 +38,7 @@ class HADataExporter extends HTMLElement {
       this._lastRenderTime = now;
       return;
     }
-    if (now - (this._lastRenderTime || 0) < 5000) {
+    if (now - (this._lastRenderTime || 0) < 10000) {
       if (!this._renderScheduled) {
         this._renderScheduled = true;
         setTimeout(() => {
